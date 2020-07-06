5e2e3618a07c981340a247b1fcca008301824887
Ludovic LEVENEUR <cyb3rghostx@gmail.com>
2020-07-06 14:06:33 +0400

    Ajout page vote

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.get('/', function(req, res) {
 });
 
 // about page 
-app.get('/about', function(req, res) {
-    res.render('pages/about');
+app.get('/vote', function(req, res) {
+    res.render('pages/vote');
 });
 
 app.listen(8080);
